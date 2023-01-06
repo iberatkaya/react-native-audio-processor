7a696c75d465e980ce51bed64f86e3238f1d1fc2
iberatkaya <beratkaya1998@gmail.com>
2023-01-05 22:01:42 -0600

    feat: added RangeError for parameters

--- src/index.tsx
+++ src/index.tsx
@@ -55,6 +55,72 @@ export function processFile(
   outputFileName: string,
   options?: ProcessFileOptions
 ): Promise<string> {
+  if (
+    options?.reverb !== undefined &&
+    (options.reverb > 100 || options.reverb < 0)
+  ) {
+    throw RangeError('reverb must be between 0 and 100.');
+  }
+  if (
+    options?.delay !== undefined &&
+    (options.delay > 100 || options.delay < 0)
+  ) {
+    throw RangeError('delay must be between 0 and 100.');
+  }
+  if (
+    options?.delayTimeInMS !== undefined &&
+    (options.delayTimeInMS > 2000 || options.delayTimeInMS < 0)
+  ) {
+    throw RangeError('delayTimeInMS must be between 0 and 2000.');
+  }
+  if (
+    options?.delayFeedback !== undefined &&
+    (options.delayFeedback > 100 || options.delayFeedback < -100)
+  ) {
+    throw RangeError('delayFeedback must be between -100 and 100.');
+  }
+  if (
+    options?.delayLowPassCutoff !== undefined &&
+    options.delayLowPassCutoff < 10
+  ) {
+    throw RangeError('delayLowPassCutoff must be larger than 10.');
+  }
+  if (
+    options?.distortionAmount !== undefined &&
+    (options.distortionAmount > 100 || options.distortionAmount < 0)
+  ) {
+    throw RangeError('distortionAmount must be between -100 and 100.');
+  }
+  if (
+    options?.distortionGain !== undefined &&
+    (options.distortionGain > 20 || options.distortionGain < -80)
+  ) {
+    throw RangeError('distortionGain must be between -80 and 20.');
+  }
+  if (
+    options?.pitchAmount !== undefined &&
+    (options.pitchAmount > 2400 || options.pitchAmount < -2400)
+  ) {
+    throw RangeError('pitchAmount must be between -2400 and 2400.');
+  }
+  if (
+    options?.pitchOverlap !== undefined &&
+    (options.pitchOverlap > 32 || options.pitchOverlap < 3)
+  ) {
+    throw RangeError('pitchOverlap must be between 3 and 32.');
+  }
+  if (
+    options?.pitchRate !== undefined &&
+    (options.pitchRate > 32 || options.pitchRate < 1 / 32)
+  ) {
+    throw RangeError('pitchOverlap must be between 1/32 and 32.');
+  }
+  if (
+    options?.playRate !== undefined &&
+    (options.playRate > 4 || options.playRate < 0.25)
+  ) {
+    throw RangeError('playRate must be between 0.25 and 4.');
+  }
   return AudioProcessor.processFile(filePath, outputFileName, options);
 }
 
